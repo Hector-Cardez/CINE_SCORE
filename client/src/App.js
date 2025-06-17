7ff4554939d68d000eede9d3826bdbcebdb85a3f
Hector-Cardez <carvajalhh@gmail.com>
2025-06-16 21:55:26 -0400

    remiving google btn

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google"; // Import GoogleOAuthProvider
-import GlobalStyles from "./components/GlobalStyles";
+//import GlobalStyles from "./components/GlobalStyles";
 import NavBar from "./components/NavBar";
 import PopularMovies from "./components/PopularMovies";
 import TopRatedMovies from "./components/TopRatedMovies";
@@ -14,7 +14,9 @@ import MoviePage from "./components/MoviePage";
 import Footer from "./components/Footer";
 
 const App = () => (
-  <GoogleOAuthProvider clientId="972191320332-v7cc469r7trk8s1f9ll159tc8r9jq17k.apps.googleusercontent.com">
+  // <GoogleOAuthProvider clientId="972191320332-v7cc469r7trk8s1f9ll159tc8r9jq17k.apps.googleusercontent.com">
+
+  <div>
     <Router>
       <div>
         <GlobalStyles />
@@ -33,7 +35,8 @@ const App = () => (
       </div>
     </Router>
     <Footer />
-  </GoogleOAuthProvider>
+  </div>
+  //</GoogleOAuthProvider>
 );
 
 export default App;
